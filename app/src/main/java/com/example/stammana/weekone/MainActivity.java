4aaae47aa71b0a104bf78eaf77c835e57c13813e
sowmika <sowmika148@gmail.com>
2018-11-14 20:26:40 +0530

    Display Welcome message with the given user name

--- app/src/main/java/com/example/stammana/weekone/MainActivity.java
+++ app/src/main/java/com/example/stammana/weekone/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.stammana.weekone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submit(View view) {
+        TextView textView = (TextView)findViewById(R.id.welcomeMessage);
+        EditText editText = (EditText)findViewById(R.id.userName);
+        textView.setText("Hello " + editText.getText().toString());
     }
 }
